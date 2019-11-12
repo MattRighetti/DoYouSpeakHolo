0588044dae4026f5528c1a8a70a9b827eb131d2a
Luca Terracciano <terraccianoluca@gmail.com>
2019-11-12 14:07:48 +0100

    Script to avoid objects get y < 0

---
+++ DoYouSpeakHolo/Assets/DoNotFall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotFall : MonoBehaviour
+{
+
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < 0)
+        {
+            Debug.Log("ciao");
+            Vector3 temp = new Vector3(0, - gameObject.transform.position.y, 0);
+            gameObject.transform.position += temp;
+            Debug.Log(gameObject.transform.position.y);
+            
+        }
+    }
+}
---
+++ DoYouSpeakHolo/Assets/HarvestingLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvestingLogic : MonoBehaviour
+{
+
+    private int fruitCounter = 0;
+    private string expectedFruit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        expectedFruit = "apple";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Check whether the gameObject is the one that needs to be put in the basket
+        if (other.gameObject.tag == this.expectedFruit )
+        {
+            fruitCounter++;
+            Debug.Log("Counting " + fruitCounter);
+        }
+
+    }
+
+    void SetExpectedFruit(string expectedFruit)
+    {
+        this.expectedFruit = expectedFruit;
+    }
+
+}

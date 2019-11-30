bead1fbc87102843168c5a8df772b6510d034107
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-30 17:03:25 +0100

    Minor fixes and Pooler instantiation phase

--- DoYouSpeakHolo/Assets/Scripts/ObjectPooler.cs
+++ DoYouSpeakHolo/Assets/Scripts/ObjectPooler.cs
@@ -4,80 +4,74 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class ObjectPooler : MonoBehaviour
-{
+public class ObjectPooler : MonoBehaviour {
     public static ObjectPooler SharedInstance;
-    public List<GameObject> pooledObjects;
+    public Dictionary<string, GameObject> pooledObjectsDictionary;
     public int amountToPool;
 
-    void Awake()
-    {
+    void Awake() {
         SharedInstance = this;
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        pooledObjectsDictionary = new Dictionary<string, GameObject>();
         CreateObjects();
+        Debug.Log("Testing");
+        foreach (string key in GetAllKeyInObjectDictionary()) {
+            Debug.Log(string.Format("Key={0}", key));
+        }
 
-        Debug.Log("Loading Prefabs");
-        pooledObjects = new List<GameObject>();
-        foreach (GameObject obj in pooledObjects)
-        {
-            obj.SetActive(false);
-            Debug.Log(obj.gameObject.tag);
-            pooledObjects.Add(obj);
+        foreach (GameObject obj in GetAllValuesInObjectDictionary()) {
+            Debug.Log(string.Format("Object={0}", obj.name));
+        }
+
+        if (GetPooledObject("medium_house") != null) {
+            Debug.Log("Retrieved!");
         }
     }
 
-    private void CreateObjects()
-    {
+    private void CreateObjects() {
         ObjectsEnum objects = ReadJSONFromFile();
-        Debug.Log(objects);
         Dictionary<string, string> dict = objects.GetDictionary();
 
-        foreach (KeyValuePair<string, string> entry in dict)
-        {
-            Debug.Log("loading " + entry.Value);
-            var x = Resources.Load(entry.Value) as GameObject;
-            Debug.Log(x);
-            GameObject obj = (GameObject)Instantiate(x);
-            obj.gameObject.tag = entry.Key;
+        foreach (KeyValuePair<string, string> entry in dict) {
+            GameObject obj = Instantiate(Resources.Load(entry.Value, typeof(GameObject))) as GameObject;
             obj.SetActive(false);
-            pooledObjects.Add(obj);
+            pooledObjectsDictionary.Add(entry.Key, obj);
             amountToPool++;
-            Debug.Log("Created " + entry.Key);
+        }
+    }
+
+    public List<string> GetAllKeyInObjectDictionary() {
+        List<string> keyValues = new List<string>(pooledObjectsDictionary.Keys);
+        return keyValues;
+    }
+
+    public List<GameObject> GetAllValuesInObjectDictionary() {
+        List<GameObject> values = new List<GameObject>(pooledObjectsDictionary.Values);
+        return values;
+    }
+
+    public GameObject GetPooledObject(string key) {
+        try {
+            GameObject obj = pooledObjectsDictionary[key];
+            return obj;
+        }
+        catch (KeyNotFoundException) {
+            Console.WriteLine("Dictionary doesn't have Key={0}", key);
+            return null;
         }
     }
 
     //Read and parse the JSON file
-    private ObjectsEnum ReadJSONFromFile()
-    {
+    private ObjectsEnum ReadJSONFromFile() {
         //Load the objects from JSON
-        string path = @"Assets\Resources\Prefab\objects.json";
+        string path = @"Assets/Resources/Prefab/objects.json";
         Debug.Log("Reading file at " + path);
         string jsonToParse = File.ReadAllText(path);
         Debug.Log("Json to parse " + jsonToParse);
         return JsonConvert.DeserializeObject<ObjectsEnum>(jsonToParse);
-    }
-
-    public GameObject GetPooledObject(string tag)
-    {
-        for (int i = 0; i < pooledObjects.Count; i++)
-        {
-            if (!pooledObjects[i].activeInHierarchy && tag == pooledObjects[i].tag)
-            {
-                Debug.Log("Returning " + tag);
-                return pooledObjects[i];
-            }
-        }
-        return null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 }

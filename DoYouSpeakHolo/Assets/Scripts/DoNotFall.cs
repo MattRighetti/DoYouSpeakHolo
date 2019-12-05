50b79b0c0d8cd2f164055a173bc0f9c4fc8489b5
Ibrahim El Shemy <ibrahim.elshemy@mail.polimi.it>
2019-12-05 13:25:51 +0100

    Gravity scripts updated

---
+++ DoYouSpeakHolo/Assets/Scripts/DoNotFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotFall : MonoBehaviour {
+    private Collider coll;
+    private GravityEnabler gravity;
+
+    // Start is called before the first frame update
+    void Start() {
+        gravity = gameObject.AddComponent<GravityEnabler>();
+        coll = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (gameObject.transform.position.y < 0) {
+            Vector3 temp = new Vector3(0, -gameObject.transform.position.y, 0);
+            gameObject.transform.position += temp;
+        }
+
+        if (gameObject.transform.position.y == 0) {
+            gravity.coll = coll;
+            gravity.DisableGravity();
+        }
+    }
+}
---
+++ DoYouSpeakHolo/Assets/Scripts/GravityEnabler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityEnabler : MonoBehaviour
+{
+    public Collider coll;
+    public ManipulationHandler man;
+
+    void Start()
+    {
+        coll = gameObject.GetComponent<Collider>();
+        man = gameObject.GetComponent<ManipulationHandler>();
+        man.OnManipulationEnded.AddListener(FallDown);
+    }
+
+    private void FallDown(ManipulationEventData arg0) {
+        coll = man.GetComponent<Collider>();
+        EnableGravity();
+    }
+
+    public void EnableGravity()
+    {
+        
+        coll.attachedRigidbody.useGravity = true;
+        coll.attachedRigidbody.isKinematic = false;
+    }
+
+    public void DisableGravity()
+    {
+        coll.attachedRigidbody.useGravity = false;
+        coll.attachedRigidbody.isKinematic = true;  
+    }
+    
+}

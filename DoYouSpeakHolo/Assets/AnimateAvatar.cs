3a3e4a3908fbfc0dfc5a7532af1694e616094631
Ibrahim El Shemy <ibrahim.elshemy@mail.polimi.it>
2019-12-05 17:52:11 +0100

    Animations scene 1 bis

---
+++ DoYouSpeakHolo/Assets/AnimateAvatar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateAvatar : MonoBehaviour {
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+        animator.Play("Idle");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            animator.Play("OK");
+        }
+        else if (Input.GetKeyDown(KeyCode.N)) {
+            animator.Play("Talking");
+            Restart();  
+        }
+        else if (Input.GetKeyDown(KeyCode.O)) {
+            animator.Play("KO");
+        }
+            
+    }
+
+    void Restart() {
+        animator.Play("Idle");
+    }
+}

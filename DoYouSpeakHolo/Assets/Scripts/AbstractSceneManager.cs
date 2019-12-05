181950d0e6c7a6d8332f16c91b4f001c1e9bb515
Mattia Righetti <matt95.righetti@gmail.com>
2019-12-05 11:35:22 +0100

    Updated GameManager

--- DoYouSpeakHolo/Assets/Scripts/AbstractSceneManager.cs
+++ DoYouSpeakHolo/Assets/Scripts/AbstractSceneManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 
 public abstract class AbstractSceneManager : MonoBehaviour {
+
+    private ObjectPooler Pooler;
+
     // Start is called before the first frame update
     void Start() {
+        Pooler = ObjectPooler.GetPooler();
     }
 
     public abstract void StartLearningPhase();
@@ -11,5 +15,12 @@ public abstract class AbstractSceneManager : MonoBehaviour {
     void Update() {
     
     }
-    
+
+    public void ActivateObject(string key, Vector3 position) {
+        Pooler.ActivateObject(key, position);
+    }
+
+    public void DeactivateObject(string key) {
+        Pooler.DeactivateObject(key);
+    }
 }

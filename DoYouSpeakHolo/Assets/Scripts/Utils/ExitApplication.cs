7340c51ab1c772da02bf6650f4171e606907287b
Ibrahim El Shemy <ibrahim.elshemy@mail.polimi.it>
2020-01-30 23:03:39 +0100

    ExitApplication Script + Button Attachment

---
+++ DoYouSpeakHolo/Assets/Scripts/Utils/ExitApplication.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApplication : MonoBehaviour
+{
+    public void Quit() {
+    //If we are running in a standalone build of the game
+    #if WINDOWS_UWP
+		//Quit the application
+		Application.Quit();
+    #endif
+
+     //If we are running in the editor
+    #if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    }
+}
